4d24ff6bb5d35db750e70ba91a4b6330c3413974
Vysakh Sreenivasan <diplomatv@gmail.com>
2015-05-07 18:11:23 +0530

    Initial commit

---
+++ app/components/search-box.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.TextField.extend({
+  placeholder: 'Search image for a human action',
+  classNames: ['center-align'],
+
+  observeInput: function() {
+    if (!this.get('holdValue')) {
+      var inp = this.get('value');
+      this.set('term', inp);
+    }
+  }.observes('value', 'holdValue', 'nsfw'),
+
+  didInsertElement: function(){
+    this.$().focus();
+    var val = this.get('term');
+    this.setProperties({
+      activeIndex: -1,
+      value: val
+    });
+  },
+  keyDown: function(e) {
+    var offset = e.keyCode === 38 ? -1 : e.keyCode === 40 ? 1 : 0; // up or down
+
+    var aI      = this.get('activeIndex'),
+      newIndex  = aI + offset,
+      results     = this.get('results');
+    if(offset !== 0) {
+
+      if (newIndex >= 0 && (newIndex < results.length)) {
+        var result = results[newIndex];
+
+        this.setProperties({
+          holdValue: true,
+          activeIndex: newIndex,
+          value: result.name
+        });
+      }
+      e.preventDefault();
+    }
+    else if(e.keyCode === 13){
+      this.sendAction('selectRes', results[aI].name);
+    }
+    else{
+      this.$().focus();
+      this.set('holdValue', false);
+      this.set('activeIndex', -1);
+    }
+  }
+});
